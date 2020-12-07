3cfb788480b10ad9e7297a75d70ce80b79c18cce
BD103 <59022059+BD103@users.noreply.github.com>
2020-12-07 11:55:01 -0500

    Create pip.sh

---
+++ cmd/pip.sh
@@ -0,0 +1,6 @@
+# You can also add other packages to this list
+pip install --upgrade wheel setuptools twine pytest
+# Wheel builds package into giant script
+# Setuptools helps build and export files
+# Twine uploads to PyPI
+# Pytest is the testing structure I recommend
