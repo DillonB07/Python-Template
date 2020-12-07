c4dacb5c602e26d2a46c678ac896c790fb5c6459
BD103 <59022059+BD103@users.noreply.github.com>
2020-12-07 11:22:04 -0500

    Create __init__.py

---
+++ pypackage/__init__.py
@@ -0,0 +1,9 @@
+'''
+This file gets run as soon as someone runs "import pypackage"
+Use this to link submodules, or you could just paste all your code here
+Check with template.md for more info.
+'''
+
+import module_a
+
+print("PyPackage Loaded")
